d4f34b6c91237e5d205791047ea7209c2f113e10
Marwan Hisham <marwanhiisham@outlook.com>
2022-05-11 21:00:23 +0200

    header , install react-router-dom@latest

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,86 @@
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import {
+  MdRssFeed,
+  MdPeopleAlt,
+  MdMenuBook,
+  MdOutlineChatBubble,
+  MdNotifications,
+  MdSearch,
+  MdArrowUpward,
+} from "react-icons/md";
+
+const NAV_ITEMS = [
+  {
+    icon: <MdRssFeed />,
+    text: "feed",
+  },
+  {
+    icon: <MdPeopleAlt />,
+    text: "network",
+  },
+  {
+    icon: <MdMenuBook />,
+    text: "jobs",
+  },
+  {
+    icon: <MdOutlineChatBubble />,
+    text: "chat",
+  },
+  {
+    icon: <MdNotifications />,
+    text: "notices",
+  },
+];
 
 const Header = () => {
   return (
-    <header>
-      <Logo />
+    <header className="shadow-md">
+      <nav className="flex items-center justify-evenly space-x-10 text-black bg-white lg:w-4/5 w-full px-2 py-4 mx-auto ">
+        <a href="/">
+          <Logo />
+        </a>
+        <nav className="flex space-x-5 lg:space-x-10">
+          {NAV_ITEMS.map((item, index) => (
+            <a
+              href="/"
+              key={index}
+              className="flex flex-col relative space-y-1 items-center text-xs group"
+            >
+              <span className="text-3xl md:text-2xl text-black group-hover:text-blue transition-all">
+                {item.icon}
+              </span>
+              <p className="uppercase hidden md:block group-hover:text-blue transition-all">
+                {item.text}
+              </p>
+            </a>
+          ))}
+        </nav>
+        <div className="flex-1 xl:flex items-center hidden">
+          <MdSearch className="text-4xl text-blue" />
+          <input
+            type="text"
+            className="w-full px-5 py-3 outline-none bg-gray bg-opacity-5 rounded-md"
+            placeholder="Search"
+          />
+        </div>
+        <a href="/" className="md:flex items-center space-x-3 hidden">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            alt="User's Avatar"
+            className="rounded-full w-12"
+          />
+          <div className="text-xs space-y-1">
+            <p>M.HISHAM</p>
+            <span className="flex flex-col text-gray">
+              <p>3469 views today</p>
+            </span>
+            <span className="text-green flex items-center">
+              <p>+203</p>
+              <MdArrowUpward />
+            </span>
+          </div>
+        </a>
+      </nav>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
+    colors: {
+      black: "#181818",
+      white: "#FFFFFF",
+      background: "#F7F9FB",
+      gray: "#747474",
+      green: "#02B033",
+      blue: "#0275B1",
+    },
     extend: {},
   },
   plugins: [],
