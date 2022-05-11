0b55c642bc0a8b7866a79cbc9ac36045460a2f1e
Marwan Hisham <marwanhiisham@outlook.com>
2022-05-11 17:05:40 +0200

    '*.svg' index.d.ts add

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
